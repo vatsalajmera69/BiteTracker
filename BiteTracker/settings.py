437cfab662454960cc6ffb93c114a0db41007c8f
Jash Doshi <jashdoshi279@gmail.com>
2025-07-08 23:03:17 +0530

    update

--- BiteTracker/settings.py
+++ BiteTracker/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'BiteTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jvpd',
+        'USER': 'root',  
+        'PASSWORD': 'Jashdoshi7$',  
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
